cd05eaf5f7034da0cea2e833bbf95ae349d7192f
Federico <Federicocerilli04@gmail.com>
2026-04-24 18:34:54 +0200

    Creazione Progetto

---
+++ TorneiCalcistici/src/main/java/Progetto/TorneiCalcisticiApplication.java
@@ -0,0 +1,13 @@
+package Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneiCalcisticiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneiCalcisticiApplication.class, args);
+	}
+
+}
