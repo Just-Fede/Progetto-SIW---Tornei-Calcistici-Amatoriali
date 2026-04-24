8c7fb80132da5c141b18e6bc1a43e99b43e02c3e
Federico <Federicocerilli04@gmail.com>
2026-04-24 21:18:18 +0200

    Creato primo controller

---
+++ TorneiCalcistici/src/main/java/it/uniroma3/siw/controller/TorneoController.java
@@ -0,0 +1,33 @@
+package it.uniroma3.siw.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import it.uniroma3.siw.model.Torneo;
+import it.uniroma3.siw.service.TorneoService;
+
+@Controller
+public class TorneoController {
+
+	private TorneoService service;
+	
+	@GetMapping("/tornei/{id}")
+	public String show (@PathVariable("id") int id, Model model)
+	{
+		model.addAttribute("torneo",this.service.findById(id));
+		return "tornei/show";
+	}
+	
+	
+	@GetMapping("/tornei")
+	public String list(Model model)
+	{
+		List<Torneo> allTornei = this.service.findAll();
+		model.addAttribute("tornei", allTornei);
+		return "movies/list";
+	}
+}
